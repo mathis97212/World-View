ac2ab8cfb5b87c29bf25ee7907f1d99e6c541e52
diallo_x <diallo_x@etna-alternance.net>
2023-11-23 13:49:25 +0100

    first push

---
+++ world-view/src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+'use client'
+const Header = ({}) => {
+    return <header className="navbar">
+        <div className="navbar-start">🌍</div>
+
+
+
+
+        <div className="navbar-end">🌍
+            <div>Home</div>
+        
+        
+        
+        
+        </div>
+
+    </header>
+};
+
+export default Header;
