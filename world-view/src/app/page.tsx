e0c9606ae76353ee94b6c9f33ef4d7a808075f2e
diallo_x <diallo_x@etna-alternance.net>
2023-11-24 11:58:18 +0100

    début du composant countrycard

--- world-view/src/app/page.tsx
+++ world-view/src/app/page.tsx
@@ -2,11 +2,17 @@
 import Head from "next/head"
 import { Inter } from "next/font/google"
 import Navbar from "@/components/Navbar"
+import CountryCard from "@/components/CountryCard";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <section className="">
+            {countries.map((country, index) => (
+                <CountryCard key={index} name={country.name} flag={country.flag} detail={country.detail} />
+            ))}
+        </section>
     </>
   );
 };
---
+++ world-view/src/components/CountryCard.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+import { useState } from "react";
+
+const CountryCard = ({name, flag, detail}) => {
+
+    return(
+
+    );
+};
+
+export default CountryCard
